78dfe602b44b6838221edf53cdd25090d3d9a92e
Nikita Savelev <48187086+SavesNick@users.noreply.github.com>
2019-07-08 19:30:01 -0400

    Created hidestudents function and modified showPage function's index's and added hideStudents function to showPage function

--- js/script.js
+++ js/script.js
@@ -6,8 +6,7 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 const listItems = document.querySelectorAll('li');
 const studentsPerPage = 10
-const list 
-const page
+
 /***
    Add your global variables that store the DOM elements you will
    need to reference and/or manipulate.
@@ -22,27 +21,40 @@ const page
 const showPage(list, page) => {
   for (var i = 0; i < listItems.length; i+=1) {
     list[i]
+    var startI = (page * studentsPerPage) - studentsPerPage ;
+    var finishI = (page * studentsPerPage);
+  hideStudents();
+  for(let i = startI; i < finishI;i += 1)
+  if(list[i]){
+    list[i]
+      list[i].style.display = "none"
+    };
   };
-  var startI = (page * studentsPerPage) - studentsPerPage ;
-  var finishI = (page * studentsPerPage);
 };
 
-/***
+//hides the listItems
+const hideStudents() => {
+for (let i = 0;i < listItems.length; i += 1);
+listItems[i].style.display = "none"
+};  
 
-"I" stands for index
 
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
 
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
+/*** 
+ "I" stands for index
+
+  Create the `showPage` function to hide all of the items in the
+ list except for the ten you want to show.
+
+  Pro Tips:
+  - Keep in mind that with a list of 54 students, the last page
+   will only display four.
+  - Remember that the first student has an index of 0.
+  - Remember that a function `parameter` goes in the parens when
+   you initially define the function, and it acts as a variable
+   or a placeholder to represent the actual function `argument`
+   that will be passed into the parens later when you call or
+   "invoke" the function
 ***/
 
 
